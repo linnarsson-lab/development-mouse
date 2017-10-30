9eb7a954507081a56c6c1fd1b9323eb41a410c00
Gioele La Manno <gioelelamanno@gmail.com>
2017-10-30 12:15:40 +0100

    first commit, far from functional

---
+++ development_mouse/__init__.py
@@ -0,0 +1,10 @@
+from .path_config import paths
+from .analysis import *
+from .timepointwise import *
+from .development_L1 import *
+from .development import *
+from .primary import *
+# from .luigi_utils import *
+# from .cluster_config import cluster
+# from .memory_config import memory
+# from .skip_config import skip
