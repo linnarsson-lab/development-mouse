1cd543fc93cad97b9a4a7eb3aac02bf2811732bb
Gioele La Manno <gioelelamanno@gmail.com>
2017-11-15 18:26:57 +0100

    add lugi utils module

--- development_mouse/__init__.py
+++ development_mouse/__init__.py
@@ -5,7 +5,7 @@ from .development_L1 import *
 from .development import *
 from .primary import *
 from .velocity import *
-# from .luigi_utils import *
+from .luigi_utils import *
 from .cluster_config import cluster
 from .memory_config import memory
 from .skip_config import skip
