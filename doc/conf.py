7406f6443d111614070837e5fcf9e2dd84d20a0d
Gioele La Manno <gioelelamanno@gmail.com>
2017-11-17 11:04:39 +0100

    should avoid the full path of class in the docs

--- doc/conf.py
+++ doc/conf.py
@@ -168,8 +168,7 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+add_module_names = False
