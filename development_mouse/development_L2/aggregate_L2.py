62184354505dcbb4701566ea06ee72e076416bce
Gioele La Manno <gioelelamanno@gmail.com>
2018-01-30 23:44:17 +0100

    indentation

--- development_mouse/development_L2/aggregate_L2.py
+++ development_mouse/development_L2/aggregate_L2.py
@@ -9,38 +9,38 @@ import luigi
 
 
 class AggregateL2(luigi.Task):  # Status: Ok
-	"""
-	Summary statistics of all clusters in a new Loom file
-	"""
-	n_markers = luigi.IntParameter(default=10, description="Number of markers considered by the Aggergator")
-	n_auto_genes = luigi.IntParameter(default=6, description="Number of genes to use in the AutoAutoannotation")
+    """
+    Summary statistics of all clusters in a new Loom file
+    """
+    n_markers = luigi.IntParameter(default=10, description="Number of markers considered by the Aggergator")
+    n_auto_genes = luigi.IntParameter(default=6, description="Number of genes to use in the AutoAutoannotation")
 
-	def requires(self) -> List[luigi.Task]:
-		return dm.PoolL2()
+    def requires(self) -> List[luigi.Task]:
+        return dm.PoolL2()
 
-	def output(self) -> luigi.Target:
-		return luigi.LocalTarget(os.path.join(dm.paths().build, "PoolL2.agg.loom"))
+    def output(self) -> luigi.Target:
+        return luigi.LocalTarget(os.path.join(dm.paths().build, "PoolL2.agg.loom"))
 
-	def run(self) -> None:
-		logging = cg.logging(self)
-		with self.output().temporary_path() as out_file:
-			ds = loompy.connect(self.input().fn)
-			cg.Aggregator(self.n_markers).aggregate(ds, out_file)
-			dsagg = loompy.connect(out_file)
+    def run(self) -> None:
+        logging = cg.logging(self)
+        with self.output().temporary_path() as out_file:
+            ds = loompy.connect(self.input().fn)
+            cg.Aggregator(self.n_markers).aggregate(ds, out_file)
+            dsagg = loompy.connect(out_file)
 
-			logging.info("Computing auto-annotation of L2 Pool")
-			aa = cg.AutoAnnotator(root=dm.paths().autoannotation)
-			aa.annotate_loom(dsagg)
-			aa.save_in_loom(dsagg)
-			
+            logging.info("Computing auto-annotation of L2 Pool")
+            aa = cg.AutoAnnotator(root=dm.paths().autoannotation)
+            aa.annotate_loom(dsagg)
+            aa.save_in_loom(dsagg)
+            
             ds.close()
-			# logging.info("Computing auto-auto-annotation")
-			# n_clusters = dsagg.shape[1]
-			# (selected, selectivity, specificity, robustness) = cg.AutoAutoAnnotator(n_genes=self.n_auto_genes).fit(dsagg)
-			# dsagg.set_attr("MarkerGenes", np.array([" ".join(ds.row_attrs["Gene"][selected[:, ix]]) for ix in np.arange(n_clusters)]), axis=1)
-			# ds.close()
-			# np.set_printoptions(precision=1, suppress=True)
-			# dsagg.set_attr("MarkerSelectivity", np.array([str(selectivity[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
-			# dsagg.set_attr("MarkerSpecificity", np.array([str(specificity[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
-			# dsagg.set_attr("MarkerRobustness", np.array([str(robustness[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
-			# dsagg.close()
+            # logging.info("Computing auto-auto-annotation")
+            # n_clusters = dsagg.shape[1]
+            # (selected, selectivity, specificity, robustness) = cg.AutoAutoAnnotator(n_genes=self.n_auto_genes).fit(dsagg)
+            # dsagg.set_attr("MarkerGenes", np.array([" ".join(ds.row_attrs["Gene"][selected[:, ix]]) for ix in np.arange(n_clusters)]), axis=1)
+            # ds.close()
+            # np.set_printoptions(precision=1, suppress=True)
+            # dsagg.set_attr("MarkerSelectivity", np.array([str(selectivity[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
+            # dsagg.set_attr("MarkerSpecificity", np.array([str(specificity[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
+            # dsagg.set_attr("MarkerRobustness", np.array([str(robustness[:, ix]) for ix in np.arange(n_clusters)]), axis=1)
+            # dsagg.close()
